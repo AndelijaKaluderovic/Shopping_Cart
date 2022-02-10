1e929c4d479ec2c1ac2fbe18f795181a3f634798
AndelijaKaluderovic <andeka011@gmail.com>
2022-02-10 19:43:38 +0100

    cart-icon

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,12 @@ import './Nav.css';
 export default function Nav() {
   return (
     <div className="nav">
-        <a href=""><span>0</span></a>
+      <div className='nav-content'>
+        <span className='nav-icon'>
+          <i className='fas fa-cart-shopping'></i>
+        </span>
+        <div className='cart-item'>0</div>
+      </div>
     </div>
   )
 }
